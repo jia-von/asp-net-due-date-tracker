ca6720e13a4e25a2b93e2df32d5395af78129f96
Jia <jiavon@ualberta.ca>
2020-09-22 14:55:50 -0600

    fixed the codes according to James spec.

--- Library/Controllers/BookController.cs
+++ Library/Controllers/BookController.cs
@@ -9,122 +9,145 @@ namespace Library.Controllers
 {
     public class BookController : Controller
     {
+        /*
+         BookController : Controller class:
+            Action/View “Create”
+                Will display the form to create an object. 
+                If the appropriate parameters are supplied in the query, attempt creation of the “Book” and add it to the list.
+                If the ID is already in the list, throw an exception.
+                Success message: "You have successfully checked out {title} until {DueDate}."
+                Error Message: “Unable to check out book: {Exception.Message}.”
+            Action/View “List”
+                Render a list of all books as links that will load the “Details” Action/View.
+            Action/View “Details”
+                If no get parameter “id” was supplied, render “No book selected.”
+                If an “id” get parameter was supplied, use GetBookByID() and render:
+                "You checked out {title} on {CheckedOutDate}, and it {is/was} due on {DueDate}."
+                Use conditional rendering to make a choice about using ‘is’ or ‘was’ based on today’s date.
+                A button that will call ExtendDueDateForBookByID().
+                A button that will call DeleteBookByID().
+            Method “CreateBook()”.
+                Accepts the same parameters as the “Book” constructor.
+                Creates and adds a “Book” to the “Books” list.
+                Ensures the provided ID is unique in the list.
+                Throw an exception if the ID already exists.
+            Method “GetBookByID()”.
+                Returns the book with the given ID from the “Books” list.
+            Method “ExtendDueDateForBookByID()”.
+                Extensions are 7 days from the current date (7 days from when the user requests the extension, not 7 days past the “DueDate”).
+            Method “DeleteBookByID()”.
+                Removes the book with the given ID from the “Books” list.
+            A public static “Books” property which is a list of “Book” objects.
+                This will be replaced by a proper database on {Day 2 assignment title}.
+            */
+        static public List<Book> Books { get; set; } = new List<Book>();
+
         public IActionResult Index()
         {
-            return RedirectToAction("Create");
+            return RedirectToAction("List");
         }
-
-        /*
-         Action/View “Create”
-            Will display the form to create an object. 
-            If the appropriate parameters are supplied in the query, attempt creation of the “Book” and add it to the list.
-            If the ID is already in the list, throw an exception.
-            Success message: "You have successfully checked out {title} until {DueDate}."
-            Error Message: “Unable to check out book: {Exception.Message}.”
-        // public Book(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
-
-         */
         public IActionResult Create(string id, string title, string author, string publicationDate, string checkedOutDate)
         {
             if (Request.Query.Count > 0)
             {
+                try
+                {
                     if (id != null && title != null && author != null && publicationDate != null && checkedOutDate != null)
                     {
-                    CreateBook(id, title, author, publicationDate, checkedOutDate);
-                    ViewBag.Success = $"You have successfully checked out {title} until {Books.Where(x => x.ID == int.Parse(id)).Single().DueDate.ToString("d")}";
+                        // Get parameters come in as a string, so we have to convert those to the data types required.
+                        Book createdBook = CreateBook(int.Parse(id), title, author, DateTime.Parse(publicationDate), DateTime.Parse(checkedOutDate));
+
+                        ViewBag.Success = $"You have successfully checked out {createdBook.Title} until {createdBook.DueDate}.";
                     }
                     else
                     {
-                        ViewBag.Error = "Not all fields have had values provided.";
-                        ViewBag.ID = id;
-                        ViewBag.Title = title;
-                        ViewBag.Author = author;
-                        ViewBag.PublicationDate = publicationDate;
-                        ViewBag.CheckedOutDate = checkedOutDate;
+                        throw new Exception("Not all fields provided for book creation.");
                     }
+                }
+                catch (Exception e)
+                {
+                    // All expected data not provided, so this will be our error state.
+                    ViewBag.Error = $"Unable to check out book: {e.Message}";
+
+                    // Store our data to re-add to the form.
+                    ViewBag.ID = id;
+                    ViewBag.BookTitle = title;
+                    ViewBag.Author = author;
+                    ViewBag.PublicationDate = publicationDate;
+                    ViewBag.CheckedOutDate = checkedOutDate;
+                }
             }
+            // else
+            // No request, so this will be our inital state.
+
             return View();
         }
-
-        // Action/View “List”
-        // Render a list of all books as links that will load the “Details” Action/View.
         public IActionResult List()
         {
-            ViewBag.BookLists = Books;
+            ViewBag.Books = Books;
+
             return View();
         }
-
-        /*
-         
-            If no get parameter “id” was supplied, render “No book selected.”
-            If an “id” get parameter was supplied, use GetBookByID() and render:
-            "You checked out {title} on {CheckedOutDate}, and it {is/was} due on {DueDate}."
-            Use conditional rendering to make a choice about using ‘is’ or ‘was’ based on today’s date.
-            A button that will call ExtendDueDateForBookByID().
-            A button that will call DeleteBookByID().
-         */
-
-        // Action/View “Details”
-        public IActionResult Details(string id, string extend, string delete)
+        public IActionResult Details(string id, string delete, string extend)
         {
-            int _id = int.Parse(id);
             IActionResult result;
 
-            if(extend != null)
+            // If ID wasn't provided, or if it won't parse to an int, or the ID doesn't exist.
+            if (id == null || !int.TryParse(id, out int temp) || Books.Where(x => x.ID == int.Parse(id)).Count() < 1)
             {
-                ExtendDueDateForBookByID(_id);
-                result = RedirectToAction("List");
-            }
-            else if (delete != null)
-            {
-                DeleteBookByID(_id);
-                result = RedirectToAction("List");
+                ViewBag.Error = "No book selected.";
+                result = View();
             }
             else
             {
-                ViewBag.BookDetail = GetBookByID(_id);
-                result = View();
+                if (delete != null)
+                {
+                    DeleteBookByID(int.Parse(id));
+                    result = RedirectToAction("List");
+                }
+                else
+                {
+                    if (extend != null)
+                    {
+                        ExtendDueDateForBookByID(int.Parse(id));
+                    }
+                    ViewBag.Book = GetBookByID(int.Parse(id));
+                    result = View();
+                }
             }
-
             return result;
         }
-
-        // Method “CreateBook()”.
-        // Accepts the same parameters as the “Book” constructor.
-        public void CreateBook(string id, string title, string author, string publicationDate, string checkedOutDate)
+        public Book CreateBook(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
         {
-            // Ensures the provided ID is unique in the list.
-            if (Books.Any(x => x.ID == int.Parse(id)))
+            /*
+            Accepts the same parameters as the “Book” constructor.
+            Creates and adds a “Book” to the “Books” list.
+            Ensures the provided ID is unique in the list.
+            Throw an exception if the ID already exists.
+            */
+            if (Books.Where(x => x.ID == id).Count() > 0)
             {
-                // Throw an exception if the ID already exists.
-                throw new Exception("Unique ID already exists." );
+                throw new Exception("That ID already exists.");
             }
-            // Creates and adds a “Book” to the “Books” list.
-            Books.Add(new Book(int.Parse(id), title, author, DateTime.Parse(publicationDate), DateTime.Parse(checkedOutDate)));
-        }
 
-        // Method “GetBookByID()”.
-        // Returns the book with the given ID from the “Books” list.
+            Book newBook = new Book(id, title, author, publicationDate, checkedOutDate);
+            Books.Add(newBook);
+            return newBook;
+        }
         public Book GetBookByID(int id)
         {
-            return Books.Where(x => x.ID == id).SingleOrDefault();
+            // Returns the book with the given ID from the “Books” list.
+            return Books.Where(x => x.ID == id).Single();
         }
-
-        // Extensions are 7 days from the current date (7 days from when the user requests the extension, not 7 days past the “DueDate”).
         public void ExtendDueDateForBookByID(int id)
         {
-            Books.Where(x => x.ID == id).SingleOrDefault().DueDate = DateTime.Now.AddDays(7);
+            // Extensions are 7 days from the current date (7 days from when the user requests the extension, not 7 days past the “DueDate”).
+            GetBookByID(id).DueDate = DateTime.Now.Date.AddDays(7);
         }
-
-
-        // Removes the book with the given ID from the “Books” list.
         public void DeleteBookByID(int id)
         {
-            Books.RemoveAll(x => x.ID == id);
+            // Removes the book with the given ID from the “Books” list.
+            Books.Remove(Books.Where(x => x.ID == id).Single());
         }
-
-        // A public static “Books” property which is a list of “Book” objects.
-        // This will be replaced by a proper database on {Day 2 assignment title}.
-        public static List<Book> Books { get; set; } = new List<Book>();
     }
 }
